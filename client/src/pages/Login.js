1a962548b663a08d8b260a19b57c509e654c881e
Matthew-Harvill <mah170000@utdallas.edu>
2022-03-30 21:39:18 -0500

    login and register pages work

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,58 @@
-const Login = () => {
+import React, { useContext, useState } from "react";
+import AppContext from "../components/AppContext";
+
+export default function Login() {
+  const { setPage } = useContext(AppContext);
+  const [user, setUser] = useState({ username: "", password: "" });
+
+  const handleChange = (e) => {
+    console.log("character typed", e.target);
+
+    const { name, value } = e.target;
+    setUser((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = async () => {
+    const response = await fetch("/login", {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.status !== 200) {
+      alert("Incorrect Username and/or Password");
+      setUser((prevState) => ({ ...prevState, password: "" }));
+    }
+  };
+
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div style={{ display: "flex" }}>
+      <textarea
+        onChange={handleChange}
+        value={user.username}
+        autoFocus
+        autoComplete="off"
+        name="username"
+        placeholder="Username..."
+      ></textarea>
+      <textarea
+        onChange={handleChange}
+        value={user.password}
+        name="password"
+        placeholder="Password..."
+      ></textarea>
+      <button onClick={handleSubmit}>Login</button>
+
+      <div style={{ display: "block" }}>
+        <p
+          style={{ textDecoration: "underline", cursor: "pointer" }}
+          onClick={() => {
+            setPage("register");
+          }}
+        >
+          Register
+        </p>
+      </div>
     </div>
   );
-};
-
-export default Login;
+}
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,9 +1,58 @@
-const Register = () => {
+import React, { useContext, useState } from "react";
+import AppContext from "../components/AppContext";
+
+export default function Register() {
+  const { setPage } = useContext(AppContext);
+  const [user, setUser] = useState({ username: "", password: "" });
+
+  const handleChange = (e) => {
+    console.log("character typed", e.target);
+
+    const { name, value } = e.target;
+    setUser((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSubmit = async () => {
+    const response = await fetch("/register", {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.status !== 200) {
+      alert("Error Registering User");
+      setUser((prevState) => ({ ...prevState, password: "" }));
+    }
+  };
+
   return (
-    <div>
-      <h1>Register Page</h1>
+    <div style={{ display: "flex" }}>
+      <textarea
+        onChange={handleChange}
+        value={user.username}
+        autoFocus
+        autoComplete="off"
+        name="username"
+        placeholder="Username..."
+      ></textarea>
+      <textarea
+        onChange={handleChange}
+        value={user.password}
+        name="password"
+        placeholder="Password..."
+      ></textarea>
+      <button onClick={handleSubmit}>Register</button>
+
+      <div style={{ display: "block" }}>
+        <p
+          style={{ textDecoration: "underline", cursor: "pointer" }}
+          onClick={() => {
+            setPage("login");
+          }}
+        >
+          Login
+        </p>
+      </div>
     </div>
   );
-};
-
-export default Register;
+}
