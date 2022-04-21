70aa8bcaa6398e1bbb527aa7bdce48ebe47f39db
Matt-Harvill <m.harvill23@gmail.com>
2022-04-21 07:36:23 -0500

    can create game with privacy and numPlayers

--- client/src/pages/Coup.js
+++ client/src/pages/Coup.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import CoupCreateGame from "../components/CoupCreateGame";
 import { socket } from "../socket";
+import unlock from "../images/unlock.png";
+import lock from "../images/lock.png";
 import "../styles/Coup.css";
 
 export default function Coup() {
@@ -8,6 +10,9 @@ export default function Coup() {
   const [onlineUsers, setOnlineUsers] = useState([]);
   const [games, setGames] = useState([]);
   const [chats, setChats] = useState([]);
+
+  const [privacy, setPrivacy] = useState(unlock);
+  const [numPlayers, setNumPlayers] = useState("2");
 
   useEffect(() => {
     socket.on("coup online", (users) => {
@@ -70,11 +75,39 @@ export default function Coup() {
   };
 
   const createGame = () => {
-    socket.emit("coup createGame", "public");
+    let privacyString;
+    if (privacy === lock) {
+      privacyString = "private";
+    } else if (privacy === unlock) {
+      privacyString = "public";
+    } else {
+      alert("error creating game");
+      return;
+    }
+
+    socket.emit("coup createGame", privacyString);
   };
 
   const handleChange = (e) => {
     setNewChat(e.target.value);
+  };
+
+  const changePrivacy = () => {
+    switch (privacy) {
+      case lock:
+        setPrivacy(unlock);
+        break;
+      case unlock:
+        setPrivacy(lock);
+        break;
+      default:
+        alert("error setting privacy");
+        break;
+    }
+  };
+
+  const selectPlayers = (e) => {
+    setNumPlayers(e.target.value);
   };
 
   return (
@@ -120,12 +153,53 @@ export default function Coup() {
           >
             {games.map(displayGames)}
           </div>
+
+          <div
+            style={{
+              display: "flex",
+              width: "100%",
+              paddingLeft: 20,
+              paddingRight: 20,
+              flexDirection: "column",
+              justifyContent: "flex-start",
+              gap: 20,
+            }}
+          >
+            <div
+              style={{
+                display: "grid",
+                gridTemplateColumns: "1fr 32px",
+                gap: 10,
+                gridAutoRows: "auto",
+              }}
+            >
+              <p readOnly={true}>Number of Players</p>
+              <select
+                onChange={selectPlayers}
+                value={numPlayers}
+                style={{ height: 32 }}
+              >
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+              </select>
+              <p readOnly={true}>{privacy === unlock ? "Public" : "Private"}</p>
+              <img
+                onClick={changePrivacy}
+                src={privacy}
+                alt="privacy"
+                style={{ height: 32, width: 32, cursor: "pointer" }}
+              ></img>
+            </div>
+          </div>
+
           <button onClick={createGame} style={{ width: "100%" }}>
             Create Game
           </button>
         </div>
       </div>
-      <CoupCreateGame />
     </div>
   );
 }
